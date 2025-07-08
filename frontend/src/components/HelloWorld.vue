66ba883c3107095f1d4636d6f11db12f5adbb0e1
Jamaica Salem <jamaica.esalem@gmail.com>
2025-07-08 19:57:17 +0800

    Initial commit: Django + Vue thesis project

---
+++ frontend/src/components/HelloWorld.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { ref, onMounted } from 'vue'
+import axios from 'axios'
+
+const message = ref('Loading...')
+
+onMounted(() => {
+  axios.get('http://127.0.0.1:8000/api/hello/')
+    .then(response => {
+      message.value = response.data.message
+    })
+    .catch(error => {
+      console.error(error)
+      message.value = 'Error fetching data'
+    })
+})
+</script>
+
+<template>
+  <div>
+    <h1>{{ message }}</h1>
+  </div>
+</template>
